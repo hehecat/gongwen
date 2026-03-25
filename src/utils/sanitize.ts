7a35e501a4eded4260c43795e72992e81e368c9c
Maruko <6285343+Marukon@users.noreply.github.com>
2026-03-25 16:39:19 +0800

    Update sanitize.ts

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -17,6 +17,10 @@ const PUNCTUATION_REPLACEMENTS = new Map<string, string>([
   ['!', '！'],
   ['(', '（'],
   [')', '）'],
+  [')', '）'],
+  [')', '）'],
+  ['"', '“'],
+  ['"', '”'],
 ])
 
 export interface SanitizeResult {
